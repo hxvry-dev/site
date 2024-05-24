955b3a587598703725463474b4324c767bebe511
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-05-24 17:55:33 -0400

    General Updates to BottomBar. Removed the Link on TopBar.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import { Center, Container, Space, Text } from '@mantine/core';
+import { Center, Container, Space } from '@mantine/core';
 import { IconCode } from '@tabler/icons-react';
 import classes from './css/TopBar.module.css';
 
@@ -6,13 +6,11 @@ const TopBar = () => {
 	return (
 		<header className={classes.header}>
 			<Container size="md" className={classes.inner}>
-				<Text component="a" href="/">
-					<Center>
-						<IconCode size={28} />
-						<Space w="xs" />
-						Henry Ouellette
-					</Center>
-				</Text>
+				<Center>
+					<IconCode size={25} />
+					<Space w="xs" />
+					Henry Ouellette
+				</Center>
 			</Container>
 		</header>
 	);
