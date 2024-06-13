afc743d2a72961b1afbc805b75e8cd9a0c6c83fd
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-06-13 18:22:45 -0400

    Clean Up Social Media a bit

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Center, Container, Space } from '@mantine/core';
+import { ActionIcon, Anchor, Center, Container, Group } from '@mantine/core';
 import {
 	Icon,
 	IconBrandInstagram,
@@ -10,8 +10,8 @@ import react from 'react';
 import classes from './css/SocialMedia.module.css';
 
 interface SocialMediaProps {
-	name: string;
-	link: string;
+	label: string;
+	href: string;
 	icon: react.ForwardRefExoticComponent<
 		Omit<IconProps, 'ref'> & react.RefAttributes<Icon>
 	>;
@@ -19,18 +19,18 @@ interface SocialMediaProps {
 
 const Socials: Array<SocialMediaProps> = [
 	{
-		name: 'GitHub',
-		link: 'https://www.github.com/hxvry-dev',
+		label: 'GitHub',
+		href: 'https://www.github.com/hxvry-dev',
 		icon: IconCode,
 	},
 	{
-		name: 'Instagram',
-		link: 'https://www.instagram.com/hxvry',
+		label: 'Instagram',
+		href: 'https://www.instagram.com/hxvry',
 		icon: IconBrandInstagram,
 	},
 	{
-		name: 'Twitter / X',
-		link: 'https://www.x.com/hxvry_',
+		label: 'Twitter / X',
+		href: 'https://www.x.com/hxvry_',
 		icon: IconBrandX,
 	},
 ];
@@ -38,20 +38,27 @@ const Socials: Array<SocialMediaProps> = [
 const SocialMedia = () => {
 	return (
 		<footer className={classes.footer}>
-			<Container size="md" className={classes.inner}>
-				{Socials.map((social, index) => (
-					<Anchor
-						href={social.link}
-						className={classes.link}
-						key={index}
-					>
-						<Center inline>
-							{<social.icon />}
-							<Space w="xs" />
-							{social.name}
-						</Center>
-					</Anchor>
-				))}
+			<Container className={classes.inner}>
+				<Group>
+					{Socials.map((social, index) => (
+						<Anchor
+							href={social.href}
+							key={index}
+							className={classes.links}
+						>
+							<Center inline>
+								<ActionIcon
+									size="lg"
+									color="dimmed"
+									variant="subtle"
+								>
+									<social.icon stroke={1.5} />
+								</ActionIcon>
+								{social.label}
+							</Center>
+						</Anchor>
+					))}
+				</Group>
 			</Container>
 		</footer>
 	);
