348a58870deb10ff621f46d6b29fbfe874ef2801
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-06-13 18:21:18 -0400

    Re-Implement Tabs in TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,49 @@
-import { Center, Container, Space } from '@mantine/core';
-import { IconCode } from '@tabler/icons-react';
+import {
+	ActionIcon,
+	Anchor,
+	Burger,
+	Center,
+	Container,
+	Group,
+	Space,
+} from '@mantine/core';
+import {
+	Icon,
+	IconCode,
+	IconHome,
+	IconPaperclip,
+	IconProps,
+} from '@tabler/icons-react';
 import classes from './css/TopBar.module.css';
+import react, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDisclosure } from '@mantine/hooks';
+
+interface LinkProps {
+	href: string;
+	label: string;
+	icon: react.ForwardRefExoticComponent<
+		IconProps & react.RefAttributes<Icon>
+	>;
+}
+
+const Links: Array<LinkProps> = [
+	{
+		href: '/',
+		label: 'Home',
+		icon: IconHome,
+	},
+	{
+		href: '/resume',
+		label: 'Job Experience / Resume',
+		icon: IconPaperclip,
+	},
+];
 
 const TopBar = () => {
+	const [active, setActive] = useState(Links[0].href);
+	const [opened, { toggle }] = useDisclosure(false);
+	const nav = useNavigate();
 	return (
 		<header className={classes.header}>
 			<Container size="md" className={classes.inner}>
@@ -11,6 +52,39 @@ const TopBar = () => {
 					<Space w="xs" />
 					Henry Ouellette
 				</Center>
+				<Group>
+					{Links.map((link, index) => (
+						<Anchor
+							href={link.href}
+							key={index}
+							className={classes.links}
+							data-active={active === link.href || undefined}
+							onClick={(event) => {
+								event.preventDefault();
+								setActive(link.href);
+								nav(link.href);
+							}}
+						>
+							<Center inline>
+								<ActionIcon
+									size="lg"
+									color="dimmed"
+									variant="subtle"
+								>
+									<link.icon stroke={1.5} />
+								</ActionIcon>
+								<Space w="xs" />
+								{link.label}
+							</Center>
+						</Anchor>
+					))}
+				</Group>
+				<Burger
+					opened={opened}
+					onClick={toggle}
+					hiddenFrom="xs"
+					size="sm"
+				/>
 			</Container>
 		</header>
 	);
