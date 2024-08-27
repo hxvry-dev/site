5ca3a90cbbaf3fcc187400f537a2a49f88852511
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-08-27 18:27:52 -0400

    This is a SPA now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
-import { Container, Title } from '@mantine/core';
+import { Container, Space, Text, Title } from '@mantine/core';
+import JobExperienceCard from '../components/JobExperienceCard';
+import classes from './css/Home.module.css';
 
 const Home = () => {
 	return (
 		<Container>
-			<Title>Home</Title>
+			<Container size="lg" className={classes.about}>
+				<Title order={3}>About Me</Title>
+				<Space h={15} />
+				<Text className={classes.aboutText}>
+					Hi! My name is Henry, and welcome to my website!
+				</Text>
+			</Container>
+			<Container>
+				<Title order={3}>Jobs I've Had</Title>
+				<Space h={15} />
+				<JobExperienceCard />
+			</Container>
 		</Container>
 	);
 };
