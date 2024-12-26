b8f9cd61991a73cda5e3b155b71a800d41dc7a3a
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-12-26 03:33:40 -0500

    Pardon our dust

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import GithubRepoTable from '@/components/custom/GithubRepoTable/github-repo-tab
 export default function Home() {
 	return (
 		<>
+			<h1 className="font-bold text-accent-foreground">Please Pardon Our Dust</h1>
+			<h2>
+				This site is under <span className="font-bold text-red-700">ACTIVE CONSTRUCTION</span>, and will change
+				with future commits to the <span className="code">site</span> repository.
+			</h2>
+			<br />
 			<p>Home Page</p>
+			<br />
 			<GithubRepoTable />
 		</>
 	);
