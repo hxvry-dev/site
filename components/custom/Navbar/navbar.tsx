eb8eb83357d8bd507334a2a4031bb5af06bd1543
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-12-28 02:41:21 -0500

    more cleanup

--- components/custom/Navbar/navbar.tsx
+++ components/custom/Navbar/navbar.tsx
@@ -1,34 +1,18 @@
 'use client';
 import Link from 'next/link';
 import UISwitch from '../UISwitch/ui-switch';
-import { BadgeInfo, FileIcon, Home, SlashSquareIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { CSSProperties } from 'react';
-
-interface NavButtonProps {
-	href: string;
-	icon: JSX.Element;
-}
-
-const navButtons: NavButtonProps[] = [
-	{
-		href: '/',
-		icon: <SlashSquareIcon />,
-	},
-];
 
 const Navbar = () => {
 	return (
 		<div className="grid grid-cols-5 gap-0">
-			{navButtons.map((item) => (
-				<Button
-					asChild
-					size="icon"
-					className={`bg-background text-foreground font-mono font-bold hover:bg-accent hover:text-accent-foreground`}
-				>
-					<Link href={item.href}>[HO]</Link>
-				</Button>
-			))}
+			<Button
+				asChild
+				size="icon"
+				className={`bg-background text-foreground font-mono font-bold hover:bg-accent hover:text-accent-foreground`}
+			>
+				<Link href="/">[HO]</Link>
+			</Button>
 			<h1 className="place-self-center col-span-3">hxvry</h1>
 			<UISwitch />
 		</div>
