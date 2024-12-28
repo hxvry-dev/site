0fd27922340faa5c1acedc410d52cc3c7bcd3a46
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-12-28 02:37:52 -0500

    Cleaning up the site. 	deleted:    app/about/page.tsx 	modified:   app/page.tsx 	modified:   components/custom/Navbar/navbar.tsx 	modified:   components/custom/UISwitch/ui-switch.tsx

--- components/custom/Navbar/navbar.tsx
+++ components/custom/Navbar/navbar.tsx
@@ -1,37 +1,35 @@
 'use client';
-import {
-	Menubar,
-	MenubarItem,
-	MenubarMenu,
-	MenubarTrigger,
-	MenubarContent,
-	MenubarSeparator,
-} from '@/components/ui/menubar';
 import Link from 'next/link';
 import UISwitch from '../UISwitch/ui-switch';
+import { BadgeInfo, FileIcon, Home, SlashSquareIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { CSSProperties } from 'react';
+
+interface NavButtonProps {
+	href: string;
+	icon: JSX.Element;
+}
+
+const navButtons: NavButtonProps[] = [
+	{
+		href: '/',
+		icon: <SlashSquareIcon />,
+	},
+];
 
 const Navbar = () => {
 	return (
 		<div className="grid grid-cols-5 gap-0">
-			<Menubar className="w-fit">
-				<MenubarMenu>
-					<MenubarTrigger>Menu</MenubarTrigger>
-					<MenubarContent sideOffset={5}>
-						<MenubarItem asChild>
-							<Link href="/">Home</Link>
-						</MenubarItem>
-						<MenubarSeparator />
-						<MenubarItem asChild>
-							<Link href="/about">About Me</Link>
-						</MenubarItem>
-						<MenubarSeparator />
-						<MenubarItem asChild>
-							<Link href="/resume">Resume</Link>
-						</MenubarItem>
-					</MenubarContent>
-				</MenubarMenu>
-			</Menubar>
-			<h1 className="place-self-center col-span-3">Henry Ouellette</h1>
+			{navButtons.map((item) => (
+				<Button
+					asChild
+					size="icon"
+					className={`bg-background text-foreground font-mono font-bold hover:bg-accent hover:text-accent-foreground`}
+				>
+					<Link href={item.href}>[HO]</Link>
+				</Button>
+			))}
+			<h1 className="place-self-center col-span-3">hxvry</h1>
 			<UISwitch />
 		</div>
 	);
