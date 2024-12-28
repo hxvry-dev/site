22dae602e57418e2d2c16c3939de5d45d4e3e9bf
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2024-12-28 02:49:18 -0500

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ export default function Home() {
 	return (
 		<>
 			<Box className="w-[650px] mt-64 font-mono justify-self-center p-5 border-2 rounded-xs">
-				<h1>Hi, I'm Henry!</h1>
+				<h1>Hi, I&apos;m Henry!</h1>
 				<br />
 				<h2>
 					I like to write code, talk about cars, and play video games in my free time. Welcome to my website.
 				</h2>
 				<br />
 				<small className="text-stone-700">
-					Click the <span className="code">[HO]</span> to go back home.{' '}
-					<span className="text-stone-800">Any time.</span>
+					Click the <span className="code">[HO]</span> to go back home.
+					<span className="text-stone-800"> Any time.</span>
 				</small>
 			</Box>
 		</>
