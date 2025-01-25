09fa3edc51681bb1e90a85362185787517c741e5
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-01-24 19:14:44 -0500

    Progress 2.0

--- src/components/custom/game/components/PrestigeButton.tsx
+++ src/components/custom/game/components/PrestigeButton.tsx
@@ -4,19 +4,24 @@ import { Button } from '@/components/ui/button';
 
 const PrestigeButton = () => {
 	const [gameState, setGameState] = useAtom(gameStateAtom);
+	const newPrestigePoints = Math.floor(gameState.resources.amount / 1e7);
+
 	const handlePrestige = () => {
-		const newPrestigePoints = Math.floor(gameState.resources.amount / 1e7);
-		setGameState((state) => ({
-			...state,
-			resources: { perSecond: 0, amount: 0 },
-			upgrades: gameState.defaultUpgrades,
-			prestigePoints: state.prestigePoints + newPrestigePoints,
-		}));
+		if (gameState.prestigePoints > 0 && newPrestigePoints > 1) {
+			setGameState((state) => ({
+				...state,
+				resources: { perSecond: 0, amount: 0 },
+				upgrades: gameState.defaultUpgrades,
+				prestigePoints: state.prestigePoints + newPrestigePoints,
+			}));
+		}
 	};
 
 	return (
 		<>
-			<Button onClick={handlePrestige}>Prestige? (Prestige Points {gameState.prestigePoints})</Button>
+			<Button onClick={handlePrestige} disabled={newPrestigePoints <= 0}>
+				Prestige? (Prestige Points {gameState.prestigePoints} New Prestige Points {newPrestigePoints})
+			</Button>
 		</>
 	);
 };
