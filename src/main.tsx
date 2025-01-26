6566724c033ed334bb36b4d9dfda1fc9f9fa0204
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-01-26 17:15:37 -0500

    Finish Linting and stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import { ThemeProvider } from './components/theme-provider.tsx';
 import { BrowserRouter } from 'react-router-dom';
