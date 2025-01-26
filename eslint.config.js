2699908415a72a5c51d613fc02071340a7c79adf
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-01-26 16:49:42 -0500

    Add ESLint and import sorting

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
 	{ ignores: ['dist'] },
@@ -14,12 +15,36 @@ export default tseslint.config(
 			globals: globals.browser,
 		},
 		plugins: {
+			'simple-import-sort': simpleImportSort,
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
-			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'simple-import-sort/imports': [
+				'error',
+				{
+					groups: [
+						// React-specific imports
+						['^react'],
+						// Side effect imports
+						['^\\u0000'],
+						// Node.js builtins
+						['^node:'],
+						// Packages.
+						['^@?\\w'],
+						// Internal aliases
+						['^~'],
+						// Parent imports. Put `..` last.
+						['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+						// Other relative imports. Put same-folder imports and `.` last.
+						['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+						// Style imports
+						['^.+\\.s?css$'],
+					],
+				},
+			],
+			'simple-import-sort/exports': 'error',
 		},
 	},
 );
