309686803d1f61e80d79552cb9c22af76e924866
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-01-26 19:30:27 -0500

    Abstract upgrades away from the component to clean things up. Also implemented a prestige/upgrade mechanic that allows the base click power to be multiplied.

--- src/components/custom/game/atomFactory.ts
+++ src/components/custom/game/atomFactory.ts
@@ -1,4 +1,5 @@
 import { atomWithStorage } from 'jotai/utils';
+import { PrestigeUpgrades, Upgrades } from './constants';
 
 interface ResourceAtom {
 	amount: number;
@@ -6,11 +7,12 @@ interface ResourceAtom {
 	perSecond: number;
 }
 
-interface Upgrade {
+export interface Upgrade {
 	name: string;
 	cost: number;
 	costMult: number;
 	clickPowerIncrease: number;
+	clickPowerMult: number;
 	level: number;
 	maxLevel: number;
 	currencyPerSecond: number;
@@ -38,81 +40,13 @@ const createGameState = (initialState: GameState) => {
 
 export const initialGameState: GameState = {
 	resources: { amount: 0, clickPower: 1, perSecond: 0 },
-	upgrades: {
-		better_index_finger: {
-			name: 'Better Index Finger',
-			cost: 10,
-			costMult: 10,
-			clickPowerIncrease: 1,
-			level: 0,
-			maxLevel: 5,
-			currencyPerSecond: 0,
-			firstPurchase: false,
-			description: `You're a gamer, so you know how to spam LMB`,
-		},
-		better_gpu: {
-			name: 'Better GPU',
-			cost: 25,
-			costMult: 12.5,
-			clickPowerIncrease: 0,
-			level: 0,
-			maxLevel: 99,
-			currencyPerSecond: 0.125,
-			firstPurchase: false,
-			description: `You bought a crappy GPU from one of your buddies. It generates some currency every second. Not much, but it's somethin'.`,
-		},
-		better_cpu: {
-			name: 'Better CPU',
-			cost: 50,
-			costMult: 15,
-			clickPowerIncrease: 0,
-			level: 0,
-			maxLevel: 99,
-			currencyPerSecond: 1,
-			firstPurchase: false,
-			description: `You were able to save up enough to buy a better CPU for your "mining" rig. This generates more currency per second, but not by much. Maybe if you keep buying them...`,
-		},
-	},
+	upgrades: Upgrades,
 	prestige: {
 		count: 0,
 		cost: 1e7,
 		points: 0,
 		costMultiplier: 500,
-		upgrades: {
-			pre_carpal_tunnel: {
-				name: 'Preemptive Carpal Tunnel Surgery',
-				description: `You find it odd that the surgeon wants to charge you per finger.`,
-				cost: 1,
-				costMult: 150,
-				level: 0,
-				maxLevel: 10,
-				clickPowerIncrease: 0,
-				currencyPerSecond: 0,
-				firstPurchase: false,
-			},
-			new_key_caps: {
-				name: 'Weighted Keycaps',
-				description: `You bought these off of Aluzam because your buddy told you he was able to spam spacebar faster with them once like 5 years ago, and never thought to buy them.`,
-				cost: 5,
-				costMult: 1,
-				level: 0,
-				maxLevel: 1,
-				clickPowerIncrease: 150,
-				currencyPerSecond: 0,
-				firstPurchase: false,
-			},
-			botnet: {
-				name: 'Bot Net',
-				description: `Leetcode finally paid off.`,
-				cost: 10,
-				costMult: 1,
-				level: 0,
-				maxLevel: 3,
-				clickPowerIncrease: 250,
-				currencyPerSecond: 10,
-				firstPurchase: false,
-			},
-		},
+		upgrades: PrestigeUpgrades,
 	},
 };
 
---
+++ src/components/custom/game/constants.ts
@@ -0,0 +1,107 @@
+/*
+	name: string,
+	description: string,
+	cost: number,
+	costMult: number,
+	clickPowerIncrease: number,
+	level: number,
+	maxLevel: number,
+	currencyPerSecond: number,
+	firstPurchase: boolean,
+*/
+
+export const Upgrades = {
+	forceful_clicking: {
+		name: `Forceful Clicking`,
+		description: `Maybe if you click harder...`,
+		cost: 10,
+		costMult: 1.75,
+		clickPowerIncrease: 0.25,
+		clickPowerMult: 1,
+		level: 0,
+		maxLevel: 999,
+		currencyPerSecond: 0,
+		firstPurchase: false,
+	},
+	forceful_clicking_ii: {
+		name: `Forceful Clicking II`,
+		description: `Maybe if you click even harder...`,
+		cost: 25,
+		costMult: 2.25,
+		clickPowerIncrease: 0.35,
+		clickPowerMult: 1,
+		level: 0,
+		maxLevel: 99,
+		currencyPerSecond: 0,
+		firstPurchase: false,
+	},
+	forceful_clicking_iii: {
+		name: `Forceful Clicking III`,
+		description: `Maybe if you click EVEN harder...`,
+		cost: 50,
+		costMult: 5.25,
+		clickPowerIncrease: 0.55,
+		clickPowerMult: 1,
+		level: 0,
+		maxLevel: 99,
+		currencyPerSecond: 0,
+		firstPurchase: false,
+	},
+	patience: {
+		name: `Patience`,
+		description: `
+		They say a watched pot never boils... Well, maybe if you buy this upgrade and stare at the screen for long enough, something will happen.
+		`,
+		cost: 50,
+		costMult: 5.25,
+		clickPowerIncrease: 0,
+		clickPowerMult: 1,
+		level: 0,
+		maxLevel: 99,
+		currencyPerSecond: 0.125,
+		firstPurchase: false,
+	},
+	patience_ii: {
+		name: `Patience II`,
+		description: `
+		They say a watched pot never boils... Well, maybe if you buy this upgrade and stare at the screen for just a little longer, something will happen.
+		`,
+		cost: 75,
+		costMult: 5.25,
+		clickPowerIncrease: 0,
+		clickPowerMult: 1,
+		level: 0,
+		maxLevel: 99,
+		currencyPerSecond: 0.25,
+		firstPurchase: false,
+	},
+	patience_iii: {
+		name: `Patience III`,
+		description: `
+		They say a watched pot never boils... Well, maybe if you buy this upgrade and stare at the screen for just a tiny bit longer, something will happen.
+		`,
+		cost: 50,
+		costMult: 5.25,
+		clickPowerIncrease: 0,
+		clickPowerMult: 1,
+		level: 0,
+		maxLevel: 99,
+		currencyPerSecond: 0.5,
+		firstPurchase: false,
+	},
+};
+
+export const PrestigeUpgrades = {
+	accidental_mutation: {
+		name: 'Accidental Mutation',
+		description: `You woke up with a few extra fingers today...`,
+		cost: 1,
+		costMult: 7.5,
+		clickPowerIncrease: 0,
+		clickPowerMult: 2,
+		level: 0,
+		maxLevel: 5,
+		currencyPerSecond: 0,
+		firstPurchase: false,
+	},
+};
