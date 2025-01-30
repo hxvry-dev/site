6523be5b8d67b57968810a7d2b59ceeae3283cef
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-01-30 18:28:30 -0500

    Add progress bar that tracks how close the user is to completing their prestige.

---
+++ src/components/custom/game/components/PrestigeBar.tsx
@@ -0,0 +1,17 @@
+import { Progress } from '@/components/ui/progress';
+import { useAtom } from 'jotai';
+import { FC } from 'react';
+import { gameStateAtom } from '../atomFactory';
+
+export const PrestigeBar: FC = () => {
+	const [gameState] = useAtom(gameStateAtom);
+	const percentage: string = `${((gameState.resources.amount / gameState.prestige.cost) * 100).toFixed(2)}% of the way to Prestige # ${gameState.prestige.count + 1}`;
+	return (
+		<div>
+			<div>
+				<Progress value={(gameState.resources.amount / gameState.prestige.cost) * 100} />
+				<div className="justify-self-center">{percentage}</div>
+			</div>
+		</div>
+	);
+};
