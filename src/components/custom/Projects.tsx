9d6f0bdc475e2f6014fd20fb881fab07f946074c
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-02-20 18:06:05 -0500

    Add tabler icons too, because we need Github Icons and I refuse to use SVG.

--- src/components/custom/Projects.tsx
+++ src/components/custom/Projects.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card';
 import { NavLink } from 'react-router-dom';
 import { Button } from '../ui/button';
+import { IconBrandGithub } from '@tabler/icons-react';
 
 const linkGenerator = (slug: string, desc: string) => {
 	return (
@@ -11,13 +12,24 @@ const linkGenerator = (slug: string, desc: string) => {
 	);
 };
 
-const ProjectData = [
+interface ProjectDataProps {
+	id: number;
+	name: string;
+	description: string;
+	link: JSX.Element;
+	src: string;
+	tools: Array<string>;
+}
+
+const ProjectData: ProjectDataProps[] = [
 	{
 		id: 0,
-		name: 'Idle Game',
+		name: 'Idle Game [BETA]',
 		description:
 			'Simple Idle game that I threw together in a few days and refined over the course of a few weeks. It has 2 upgrades and a Prestige system implemented, with plans to expand upgrades further in the future. The code is available on my GitHub.',
 		link: linkGenerator('incremental', 'Idle Game'),
+		src: 'https://github.com/hxvry-dev/site/tree/main/src/components/custom/game',
+		tools: ['Jotai', 'React', 'TypeScript', 'ShadCN/UI'],
 	},
 ];
 
@@ -39,8 +51,10 @@ const Projects: FC = () => {
 								<div className="grid grid-cols-2 gap-0">
 									<div>{project.link}</div>
 									<div>
-										<Button size="icon" variant="ghost">
-											{}
+										<Button asChild>
+											<NavLink to={project.src} target="_blank">
+												{<IconBrandGithub />} Source
+											</NavLink>
 										</Button>
 									</div>
 								</div>
