ac2ddb03031a6ddadf47b27d0b03394b11791c39
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-02-26 16:48:30 -0500

    Speaking of version bump...

--- src/components/custom/game/components/version.tsx
+++ src/components/custom/game/components/version.tsx
@@ -5,7 +5,7 @@ export const Version: FC = () => {
 	return (
 		<div className="justify-self-center">
 			<NavLink to="https://github.com/hxvry-dev/site" target="_blank">
-				<small className="code mb-2">Version: 0.1.0</small>
+				<small className="code mb-2">Version: 0.1.1</small>
 			</NavLink>
 		</div>
 	);
