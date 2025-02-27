5d0504d940f2add113f3fc97754056aa932a564e
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-02-27 18:58:38 -0500

    Fixup tooltip width and theme. Also remove link from Version

--- src/components/custom/game/components/version.tsx
+++ src/components/custom/game/components/version.tsx
@@ -1,12 +1,9 @@
 import { FC } from 'react';
-import { NavLink } from 'react-router-dom';
 
 export const Version: FC = () => {
 	return (
 		<div className="justify-self-center">
-			<NavLink to="https://github.com/hxvry-dev/site" target="_blank">
-				<small className="code mb-2">Version: 0.1.1</small>
-			</NavLink>
+			<small className="code mb-2">Version: 0.1.1</small>
 		</div>
 	);
 };
