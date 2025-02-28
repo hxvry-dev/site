51c4c8ea2652935dbcb2c9d74a33296c69653b9f
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-02-28 13:24:15 -0500

    We only need one client

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ import Navbar from './components/custom/Navbar';
 import NotFound from './components/custom/NotFound';
 import Resume from './components/custom/Resume';
 import Projects from './components/custom/Projects';
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 
 const App: FC = () => {
 	return (
---
+++ src/db/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+const client = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
+const supabase = () => client;
+export default supabase;
