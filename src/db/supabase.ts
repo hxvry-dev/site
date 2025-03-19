e032c0e803d5b8c32d8e7884d5682c1018f73b03
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-03-19 17:35:15 -0400

    I made an oopsie
    Apparently, I can't do this this way, so revert back to the way it was working before, just with one file.

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,8 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import process from 'process';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL!;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY!;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 export default supabase;
