abff2cd39d2ac8e58e68cff5bfe935a7da609fbc
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-04-29 17:14:03 -0400

    Updates to Schema to make readability easier (?). Also revert changes made to incremental.

--- src/components/custom/game/schema.ts
+++ src/components/custom/game/schema.ts
@@ -50,45 +50,42 @@ export const zGameStateSchema = z
 // V2 Implementation Below
 
 const upgradeTypeEnum = z.enum(['base', 'prestige', 'temp']);
-const gameUpgradeSchema = z.object({
-	upgrade_id: z.string().uuid(),
-	upgrade_type: upgradeTypeEnum,
-	upgrade_name: z.string().nonempty(),
-	upgrade_desc: z.string().nonempty(),
-	cost: z.number().nonnegative(),
-	cost_mult: z.number().nonnegative(),
-	level_max: z.number().int().min(0).max(999),
-	cpc_inc: z.number().nonnegative(),
-	cpc_mult_inc: z.number().nonnegative(),
-	currency_per_second_inc: z.number().nonnegative(),
-});
-const userUpgradeSchema = z.object({
-	user_id: z.string().uuid(),
-	upgrade_id: z.string().uuid(),
-	level_current: z.number().int().min(0),
-	purchased_at: z.string(),
-	prestige_num: z.number(),
-	id: z.string().uuid(),
-});
-const userSchema = z.object({
-	user_id: z.string().uuid(),
-	created_at: z.string(),
-	currency_balance: z.number().nonnegative(),
-	currency_per_second: z.number().nonnegative(),
-	prestige_points_balance: z.number().nonnegative(),
-	num_times_prestiged: z.number().nonnegative(),
-	prestige_cost: z.number().nonnegative(),
-});
 
 export type tGameStateV2 = z.infer<typeof GameStateV2>;
-export type tUsers = z.infer<typeof userSchema>;
-export type tUserUpgrades = z.infer<typeof userUpgradeSchema>;
-export type tGameUpgrade = z.infer<typeof gameUpgradeSchema>;
-
 export const GameStateV2 = z
 	.object({
-		user: userSchema,
-		userUpgrades: z.array(userUpgradeSchema),
-		upgrades: z.array(gameUpgradeSchema),
+		user: z.object({
+			user_id: z.string().uuid(),
+			created_at: z.string(),
+			currency_balance: z.number().nonnegative(),
+			currency_per_second: z.number().nonnegative(),
+			prestige_points_balance: z.number().nonnegative(),
+			num_times_prestiged: z.number().nonnegative(),
+			prestige_cost: z.number().nonnegative(),
+		}),
+		userUpgrades: z.array(
+			z.object({
+				user_id: z.string().uuid(),
+				upgrade_id: z.string().uuid(),
+				level_current: z.number().int().min(0),
+				purchased_at: z.string(),
+				prestige_num: z.number(),
+				id: z.string().uuid(),
+			}),
+		),
+		upgrades: z.array(
+			z.object({
+				upgrade_id: z.string().uuid(),
+				upgrade_type: upgradeTypeEnum,
+				upgrade_name: z.string().nonempty(),
+				upgrade_desc: z.string().nonempty(),
+				cost: z.number().nonnegative(),
+				cost_mult: z.number().nonnegative(),
+				level_max: z.number().int().min(0).max(999),
+				cpc_inc: z.number().nonnegative(),
+				cpc_mult_inc: z.number().nonnegative(),
+				currency_per_second_inc: z.number().nonnegative(),
+			}),
+		),
 	})
 	.strict();
