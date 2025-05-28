5c1cbf5c74d69470de0ecd2ce6e0416541f8ee77
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-05-28 14:55:59 -0400

    V2 `GameStats` and `ClickerButton`

--- src/components/custom/game/components/ClickerButton.tsx
+++ src/components/custom/game/components/ClickerButton.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { useAtom } from 'jotai';
 
-import { gameStateAtom } from '../atomFactory';
+import { gameStateAtom, gameStateV2Atom } from '../atomFactory';
 
 import { Button } from '@/components/ui/button';
 
@@ -23,9 +23,34 @@ export const ClickerButton: FC = () => {
 			},
 		}));
 	};
+
+	// V2 Here
+
+	const [gameStateV2, setGameStateV2] = useAtom(gameStateV2Atom);
+	const clickValueV2: number = gameStateV2.user.currency_per_click * gameStateV2.user.currency_per_click_mult;
+
+	const handleClickV2 = () => {
+		setGameStateV2((state) => {
+			return {
+				...state,
+				user: {
+					...state.user,
+					currency_balance:
+						state.user.currency_balance +
+						state.user.currency_per_click * state.user.currency_per_click_mult,
+				},
+			};
+		});
+	};
+
 	return (
-		<Button onClick={handleClick} className="flex w-full font-mono">
-			Click me! (+{clickValue.toFixed(2)})
-		</Button>
+		<div>
+			<Button onClick={handleClick} className="flex w-full font-mono">
+				Click me! (+{clickValue.toFixed(2)})
+			</Button>
+			<Button onClick={handleClickV2} className="flex w-full font-mono">
+				v2 Click Me! (+{clickValueV2.toFixed(2)})
+			</Button>
+		</div>
 	);
 };
--- src/components/custom/game/components/GameStats.tsx
+++ src/components/custom/game/components/GameStats.tsx
@@ -2,8 +2,9 @@ import { FC } from 'react';
 
 import { useAtom } from 'jotai';
 
-import { gameStateAtom } from '../atomFactory';
+import { gameStateAtom, gameStateV2Atom } from '../atomFactory';
 import { newPrestigePoints } from '../util/util';
+import { handleNewPrestigePoints } from '../util/v2Functions';
 
 export const GameStats: FC = () => {
 	const [gameState] = useAtom(gameStateAtom);
@@ -38,6 +39,46 @@ export const GameStats: FC = () => {
 				<div className="text-sm border-b-2 font-bold content-center w-fit">
 					{gameState.prestige.prestigeCost}
 				</div>
+			</div>
+		</div>
+	);
+};
+
+// V2 Here
+
+export const GameStatsV2: FC = () => {
+	const [gameState] = useAtom(gameStateV2Atom);
+	return (
+		<div className="mt-4 p-5 border-2 font-mono justify-self-center grid grid-cols-2 gap-4">
+			<div className="grid grid-cols-[max-content_max-content] gap-x-4 gap-y-2 mt-4">
+				<div className="text-sm border-b-2 italic w-fit">Resources: </div>
+				<div className="text-sm border-b-2 font-bold content-center w-fit">
+					{gameState.user.currency_balance.toFixed(2)}
+				</div>
+				<div className="text-sm border-b-2 italic w-fit">Current Click Power: </div>
+				<div className="text-sm border-b-2 font-bold content-center w-fit">
+					+{gameState.user.currency_per_click.toFixed(2)}
+				</div>
+				<div className="text-sm border-b-2 italic w-fit">Current Click Power Multiplier:</div>
+				<div className="text-sm border-b-2 font-bold content-center w-fit">
+					{gameState.user.currency_per_click_mult.toFixed(2)}x
+				</div>
+				<div className="text-sm border-b-2 italic w-fit">Currency Per Second: </div>
+				<div className="text-sm border-b-2 font-bold content-center w-fit">
+					{gameState.user.currency_per_second.toFixed(2)}
+				</div>
+			</div>
+			<div className="grid grid-cols-[max-content_max-content] gap-x-4 gap-y-2 mt-4">
+				<div className="text-sm border-b-2 italic w-fit">Prestige Points: </div>
+				<div className="text-sm border-b-2 font-bold content-center w-fit">
+					{gameState.user.prestige_points_balance}
+				</div>
+				<div className="text-sm border-b-2 italic w-fit">Prestige Points Gained on Next Prestige: </div>
+				<div className="text-sm border-b-2 font-bold content-center w-fit">
+					{handleNewPrestigePoints(gameState)}
+				</div>
+				<div className="text-sm border-b-2 italic w-fit">Cost to Prestige: </div>
+				<div className="text-sm border-b-2 font-bold content-center w-fit">{gameState.user.prestige_cost}</div>
 			</div>
 		</div>
 	);
