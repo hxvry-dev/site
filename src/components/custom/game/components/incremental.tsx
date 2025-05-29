943b343054df3b65c668969db68a91a47ebc1b06
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-05-29 16:06:25 -0400

    Remove V2 code from V1

--- src/components/custom/game/components/incremental.tsx
+++ src/components/custom/game/components/incremental.tsx
@@ -1,16 +1,8 @@
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC, useEffect, useRef } from 'react';
 
-import { Session } from '@supabase/supabase-js';
 import { useAtom } from 'jotai';
 
-import {
-	debugGameState,
-	debugModeAtom,
-	gameStateAtom,
-	gameStateV2Atom,
-	initialGameState,
-	toggleAtom,
-} from '../atomFactory';
+import { debugGameState, debugModeAtom, gameStateAtom, initialGameState, toggleAtom } from '../atomFactory';
 
 import { BuyMultiple } from './BuyMultiple';
 import { ClickerButton } from './ClickerButton';
@@ -21,19 +13,12 @@ import { PrestigeButton } from './PrestigeButton';
 import { Upgrades } from './Upgrades';
 import { Version } from './version';
 
-import { LoginForm } from '@/components/login-form';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-import supabase from '@/db/supabase';
-import { fetchAndValidateGameState } from '@/db/functions';
-
-async function handleSignOut() {
-	await supabase.auth.signOut();
-}
+import { NavLink } from 'react-router-dom';
 
 export const Incremental: FC = () => {
-	const [session, setSession] = useState<Session | null>(null);
 	const [, setGameState] = useAtom(gameStateAtom);
 	const [toggle, setToggle] = useAtom(toggleAtom);
 	const [debugMode, setDebugMode] = useAtom(debugModeAtom);
@@ -41,13 +26,12 @@ export const Incremental: FC = () => {
 	const intervalRef = useRef<NodeJS.Timeout | null>(null);
 	const lastUpdateRef = useRef(Date.now());
 
-	const [gameStateV2, setGameStateV2] = useAtom(gameStateV2Atom);
-
 	const handleToggle = () => {
 		setToggle((prev) => !prev);
 	};
 
-	/*useEffect(() => {
+	useEffect(() => {
+		document.title = 'Idle Game';
 		const updateResources = () => {
 			const now = Date.now();
 			const elapsedTime = (now - lastUpdateRef.current) / 1000; // convert to seconds
@@ -75,168 +59,109 @@ export const Incremental: FC = () => {
 				clearInterval(intervalRef.current);
 			}
 		};
-	}, [setGameState]);*/
-
-	// V2
-
-	useEffect(() => {
-		const updateResources = () => {
-			const now = Date.now();
-			const elapsedTime = (now - lastUpdateRef.current) / 1000;
-			lastUpdateRef.current = now;
-
-			setGameStateV2((state) => {
-				return {
-					...state,
-					user: {
-						...state.user,
-						currency_balance: state.user.currency_balance + state.user.currency_per_second * elapsedTime,
-					},
-				};
-			});
-		};
-		intervalRef.current = setInterval(updateResources, 1000 / 60);
-
-		return () => {
-			if (intervalRef.current) {
-				clearInterval(intervalRef.current);
-			}
-		};
-	}, [setGameStateV2]);
-
-	useEffect(() => {
-		document.title = 'Idle Game';
-		supabase.auth
-			.getSession()
-			.then(async ({ data: { session } }) => {
-				const gsv2 = await fetchAndValidateGameState();
-				if (!gsv2) return;
-				setGameStateV2(gsv2);
-				return setSession(session);
-			})
-			.catch((error) => {
-				console.error('Failed to fetch game state:', error);
-			});
-
-		const {
-			data: { subscription },
-		} = supabase.auth.onAuthStateChange((_event, session) => {
-			return setSession(session);
-		});
-
-		return () => subscription.unsubscribe();
-	}, []);
+	}, [setGameState]);
 
 	return (
 		<>
-			{!session ? (
-				<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-					<div className="w-full max-w-sm">
-						<LoginForm />
-					</div>
+			<div>
+				<h1 className="font-incremental text-2xl justify-self-center mb-16">Idle Game</h1>
+				<div className="justify-self-end pt-0">
+					<Button variant="link" asChild>
+						<NavLink to="/incremental/v2">Load V2</NavLink>
+					</Button>
 				</div>
-			) : (
-				<div>
-					<h1 className="font-incremental text-2xl justify-self-center mb-16">Idle Game</h1>
-					<div className="justify-self-end pt-0">
-						<Button variant="link" onClick={() => handleSignOut()}>
-							Sign Out
-						</Button>
-					</div>
-					<div className="justify-self-center font-mono">
-						<p className="justify-self-center mb-5">Buy Multiple Upgrades!</p>
-						<BuyMultiple />
-					</div>
-					<GameStats />
-					<div className="mt-5 px-5 max-w-fit justify-self-center" hidden={!toggle}>
-						<legend className="mb-4 font-mono justify-self-center">Upgrades</legend>
-						<Tabs defaultValue="base" className="border-2 rounded-sm p-5">
-							<TabsList className="self-center bg-background font-mono">
-								<TabsTrigger value="base" className="rounded-none">
-									Base
-								</TabsTrigger>
-								<TabsTrigger value="prestige" className="rounded-none">
-									Prestige
-								</TabsTrigger>
-							</TabsList>
-							<div className="max-w-[350px] self-center my-5 grid grid-cols-2 grid-rows-2 gap-5">
-								<ClickerButton />
-								<PrestigeButton />
-								<div className="col-span-2 grid-row-2">{<PrestigeBar />}</div>
-							</div>
-							<TabsContent value="base">{<Upgrades upgradeType="base" />}</TabsContent>
-							<TabsContent value="prestige">{<Upgrades upgradeType="prestige" />}</TabsContent>
-						</Tabs>
-					</div>
-					<div className="justify-self-center font-mono">
-						<Button
-							onClick={() => handleToggle()}
-							className={
-								!toggle
-									? 'mt-5 opacity-85 bg-green-700 hover:bg-green-700/90 text-foreground'
-									: 'mt-5 opacity-85 bg-red-800 hover:bg-red-800/90 text-foreground'
-							}
-						>
-							Toggle Upgrades{` ${!toggle ? 'ON' : 'OFF'}`}
-						</Button>
-					</div>
-					<EnterDebug />
-					{debugMode ? (
-						<div className="grid grid-flow-rows gap-4 justify-self-center mt-8 font-mono">
-							<div className="max-w-[300px] justify-self-center font-mono overflow-auto">
-								<TooltipProvider>
-									<Tooltip>
-										<TooltipTrigger asChild>
-											<Button
-												onClick={() => setGameState(debugGameState)}
-												className="opacity-85 bg-green-800 hover:bg-green-800/90 text-foreground"
-											>
-												Set Debug State?
-											</Button>
-										</TooltipTrigger>
-										<TooltipContent className="bg-background border-2 text-foreground max-w-[240px] overflow-auto">
-											This button will set a{' '}
-											<code className="bg-accent text-foreground px-0.5">GameState</code> item
-											that makes live debugging and testing easier. Please note that this change
-											is destructive as of now, and will require a full state reset to be able to
-											play the game again. This <b>will</b> be changed in a future update.
-										</TooltipContent>
-									</Tooltip>
-								</TooltipProvider>
-							</div>
-							<div className="max-w-[300px] justify-self-center font-mono overflow-auto">
-								<TooltipProvider>
-									<Tooltip>
-										<TooltipTrigger asChild>
-											<Button
-												onClick={() => {
-													setDebugMode(false);
-													setGameState(initialGameState);
-												}}
-												className="opacity-85 bg-yellow-600 hover:bg-yellow-600/90 text-foreground"
-											>
-												Leave Debug Mode?
-											</Button>
-										</TooltipTrigger>
-										<TooltipContent className="bg-background border-2 text-foreground max-w-[240px] overflow-auto">
-											<p>
-												This button leaves the debug mode. If you would like to come back, just
-												put the password back in :&#41;
-											</p>
-										</TooltipContent>
-									</Tooltip>
-								</TooltipProvider>
-							</div>
+				<div className="justify-self-center font-mono">
+					<p className="justify-self-center mb-5">Buy Multiple Upgrades!</p>
+					<BuyMultiple />
+				</div>
+				<GameStats />
+				<div className="mt-5 px-5 max-w-fit justify-self-center" hidden={!toggle}>
+					<legend className="mb-4 font-mono justify-self-center">Upgrades</legend>
+					<Tabs defaultValue="base" className="border-2 rounded-sm p-5">
+						<TabsList className="self-center bg-background font-mono">
+							<TabsTrigger value="base" className="rounded-none">
+								Base
+							</TabsTrigger>
+							<TabsTrigger value="prestige" className="rounded-none">
+								Prestige
+							</TabsTrigger>
+						</TabsList>
+						<div className="max-w-[350px] self-center my-5 grid grid-cols-2 grid-rows-2 gap-5">
+							<ClickerButton />
+							<PrestigeButton />
+							<div className="col-span-2 grid-row-2">{<PrestigeBar />}</div>
 						</div>
-					) : (
-						<></>
-					)}
-					<div className="mt-8">
-						<Version />
-						<div>{JSON.stringify(gameStateV2)}</div>
-					</div>
+						<TabsContent value="base">{<Upgrades upgradeType="base" />}</TabsContent>
+						<TabsContent value="prestige">{<Upgrades upgradeType="prestige" />}</TabsContent>
+					</Tabs>
 				</div>
-			)}
+				<div className="justify-self-center font-mono">
+					<Button
+						onClick={() => handleToggle()}
+						className={
+							!toggle
+								? 'mt-5 opacity-85 bg-green-700 hover:bg-green-700/90 text-foreground'
+								: 'mt-5 opacity-85 bg-red-800 hover:bg-red-800/90 text-foreground'
+						}
+					>
+						Toggle Upgrades{` ${!toggle ? 'ON' : 'OFF'}`}
+					</Button>
+				</div>
+				<EnterDebug />
+				{debugMode ? (
+					<div className="grid grid-flow-rows gap-4 justify-self-center mt-8 font-mono">
+						<div className="max-w-[300px] justify-self-center font-mono overflow-auto">
+							<TooltipProvider>
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<Button
+											onClick={() => setGameState(debugGameState)}
+											className="opacity-85 bg-green-800 hover:bg-green-800/90 text-foreground"
+										>
+											Set Debug State?
+										</Button>
+									</TooltipTrigger>
+									<TooltipContent className="bg-background border-2 text-foreground max-w-[240px] overflow-auto">
+										This button will set a{' '}
+										<code className="bg-accent text-foreground px-0.5">GameState</code> item that
+										makes live debugging and testing easier. Please note that this change is
+										destructive as of now, and will require a full state reset to be able to play
+										the game again. This <b>will</b> be changed in a future update.
+									</TooltipContent>
+								</Tooltip>
+							</TooltipProvider>
+						</div>
+						<div className="max-w-[300px] justify-self-center font-mono overflow-auto">
+							<TooltipProvider>
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<Button
+											onClick={() => {
+												setDebugMode(false);
+												setGameState(initialGameState);
+											}}
+											className="opacity-85 bg-yellow-600 hover:bg-yellow-600/90 text-foreground"
+										>
+											Leave Debug Mode?
+										</Button>
+									</TooltipTrigger>
+									<TooltipContent className="bg-background border-2 text-foreground max-w-[240px] overflow-auto">
+										<p>
+											This button leaves the debug mode. If you would like to come back, just put
+											the password back in :&#41;
+										</p>
+									</TooltipContent>
+								</Tooltip>
+							</TooltipProvider>
+						</div>
+					</div>
+				) : (
+					<></>
+				)}
+				<div className="mt-8">
+					<Version />
+				</div>
+			</div>
 		</>
 	);
 };
