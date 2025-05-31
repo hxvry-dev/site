807b752fbaf47e015be7f21302dfe6ac52e53b44
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-05-31 03:41:49 -0400

    Remove extraneous database functions in an effort to minimize calls.

--- src/db/functions.ts
+++ src/db/functions.ts
@@ -1,5 +1,4 @@
 import { GameStateV2, Upgrade, UserUpgrade } from '@/components/custom/game/schema';
-import { Tables } from './api';
 import { supabase } from '@/components/custom/game/components/IncrementalV2';
 
 export const getUserID = async (): Promise<string | undefined> => {
@@ -7,49 +6,9 @@ export const getUserID = async (): Promise<string | undefined> => {
 		data: { user },
 	} = await supabase.auth.getUser();
 	if (user) {
+		sessionStorage.setItem('user_id', user.id);
 		return user.id;
 	}
-};
-
-export const userUpgrades = async (): Promise<Tables<'user_upgrades'>[] | undefined> => {
-	const userID = await getUserID();
-	if (!userID) return;
-	const { data, error } = await supabase.from('user_upgrades').select('*').eq('user_id', userID);
-	if (error) {
-		console.error(`There was a problem grabbing your upgrades... Error code: ${error.code}`, error.message);
-	} else if (data) {
-		console.log('Loaded User Upgrades from DB!');
-		return data as Tables<'user_upgrades'>[];
-	}
-	return [];
-};
-
-export const loadUserFromDB = async (): Promise<Tables<'users'> | undefined> => {
-	const userID = await getUserID();
-	if (!userID) return;
-	console.log(userID);
-	const { data, error } = await supabase.from('users').select('*').eq('user_id', userID).single();
-	if (error) {
-		console.error(`There was a problem grabbing your Game State... Error code: ${error.code}`, error.message);
-	} else if (data) {
-		console.log('Loaded User from DB!');
-		return data as Tables<'users'>;
-	}
-	return data;
-};
-
-export const getUpgradesFromDB = async (): Promise<Tables<'upgrades'>[] | undefined> => {
-	await supabase.auth.getUser().then(async ({ data: { user } }) => {
-		if (!user) return;
-	});
-	const { data, error } = await supabase.from('upgrades').select('*');
-	if (error) {
-		console.error(`There was a problem grabbing the upgrades... Error code: ${error.code}`, error.message);
-	} else if (data) {
-		console.log('Loaded Upgrades from DB!');
-		return data as Tables<'upgrades'>[];
-	}
-	return [];
 };
 
 export const fetchAndValidateGameState = async (): Promise<GameStateV2 | undefined> => {
@@ -80,6 +39,7 @@ export const fetchAndValidateGameState = async (): Promise<GameStateV2 | undefin
 
 		const validated = GameStateV2.safeParse(gameStateV2);
 		if (validated.success) {
+			console.log('Validated GameStateV2');
 			return gameStateV2;
 		}
 	} catch (error) {
@@ -119,30 +79,19 @@ export const syncGameState = async (gameState: GameStateV2): Promise<GameStateV2
 	}
 };
 
-export const calculateLevel = async (upgradeID: string): Promise<number | null> => {
-	try {
-		const { data, error } = await supabase
-			.from('user_upgrades')
-			.select('max(level_current)')
-			.eq('upgrade_id', upgradeID)
-			.single();
-		if (error) {
-			console.error(`Error fetching user upgrades: ${error.code}`, error.message);
-			return null;
-		}
-		const maxLevel = data.max.map((upgrade) => parseInt(upgrade.level_current))[0];
-		return maxLevel || null;
-	} catch (error) {
-		console.error('An unexpected error occurred:', error);
-		return null;
-	}
+export const calculateLocalLevel = (upgrade: Upgrade, gameState: GameStateV2): number => {
+	const levels = gameState.userUpgrades
+		.filter((u) => u.upgrade_id === upgrade.upgrade_id)
+		.map((u) => u.level_current);
+	const current_level = Math.max(...levels, 0);
+	return current_level;
 };
 
 export const upsertUserUpgrade = async (upgrade: Upgrade, gameState: GameStateV2) => {
 	const userID: string | undefined = await getUserID();
 	if (!userID) return;
 	return await supabase.from('user_upgrades').upsert({
-		level_current: await calculateLevel(upgrade.upgrade_id),
+		level_current: calculateLocalLevel(upgrade, gameState),
 		prestige_num: gameState.user.num_times_prestiged,
 		upgrade_id: upgrade.upgrade_id,
 		user_id: userID,
@@ -152,10 +101,7 @@ export const upsertUserUpgrade = async (upgrade: Upgrade, gameState: GameStateV2
 export const purchaseUserUpgrade = async (upgrade: Upgrade, gameState: GameStateV2) => {
 	let result: Array<UserUpgrade> = [];
 	const userID = await getUserID();
-	const levels = gameState.userUpgrades
-		.filter((u) => u.user_id === userID && u.upgrade_id === upgrade.upgrade_id)
-		.map((u) => u.level_current);
-	const current_level = Math.max(...levels);
+	const current_level: number = calculateLocalLevel(upgrade, gameState);
 
 	result.push({
 		level_current: current_level,
@@ -164,5 +110,6 @@ export const purchaseUserUpgrade = async (upgrade: Upgrade, gameState: GameState
 		user_id: userID!,
 	});
 
+	console.log(result);
 	return result;
 };
