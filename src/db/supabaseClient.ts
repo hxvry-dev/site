a428e07e0d0b9ebd6d3ddc6ba3e00df54129f60a
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-06-10 18:14:44 -0400

    Singleton Supabase Client?

--- src/db/supabaseClient.ts
+++ src/db/supabaseClient.ts
@@ -1,8 +1,5 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-let supabase: SupabaseClient | null = null;
-if (!supabase) {
-	supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
-}
+const supabase: SupabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 
 export { supabase };
