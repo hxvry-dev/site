78b93c39e250ca090cf38c2d08424ebc9cfe0f4f
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-06-19 02:28:52 -0400

    Implement `ThemeDropdown`

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,22 +1,23 @@
 import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import UISwitch from './UISwitch';
-
 import { Button } from '@/components/ui/button';
+import { ThemeDropdown } from './ThemeDropdown';
 
 const Navbar: FC = () => {
 	return (
-		<div className="font-bold font-mono grid grid-cols-5 gap-0">
+		<div className="font-bold font-mono grid grid-cols-5 gap-2">
 			<Button
 				asChild
 				size="icon"
 				className={`bg-background text-foreground hover:bg-accent hover:text-accent-foreground`}
 			>
-				<NavLink to="/">[HO]</NavLink>
+				<NavLink to="/" className="rounded-none">
+					[HO]
+				</NavLink>
 			</Button>
 			<h1 className="place-self-center col-span-3">hxvry</h1>
-			<UISwitch />
+			<ThemeDropdown />
 		</div>
 	);
 };
