fd9eb630acee45b23b83134056749c7e2f21582f
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-06-26 00:44:17 -0400

    Changes/fixes a few things.
    - Fixes Local CSS imports, because we're using local font files in this project.
    - Update `Version` to include a Tooltip that shows the version history between [v2.0](https://github.com/hxvry-dev/site/releases/tag/v2.0.0) and now.
    - Clean up `BuyMultiple` and `BuyMultipleV2` to no longer include a background color when selected.

--- src/components/custom/game/components/version.tsx
+++ src/components/custom/game/components/version.tsx
@@ -1,9 +1,25 @@
 import { FC } from 'react';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Button } from '@/components/ui/button';
+import { NavLink } from 'react-router-dom';
 
 export const Version: FC = () => {
 	return (
-		<div className="justify-self-center">
-			<small className="code mb-2">Version: 2.5.0</small>
+		<div className="justify-self-center mt-5">
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<small className="code mb-2">Version: 2.5.0</small>
+					</TooltipTrigger>
+					<TooltipContent className="bg-background border-2 text-foreground max-w-[240px] overflow-auto">
+						<Button asChild variant="ghost" className="rounded-md">
+							<NavLink to="https://github.com/hxvry-dev/site/compare/v2.0.0...main" target="_blank">
+								Changelog
+							</NavLink>
+						</Button>
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
 		</div>
 	);
 };
