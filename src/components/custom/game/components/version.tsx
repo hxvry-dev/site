93af301010b6f0162d274d8b8c0e60fdae145f14
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2025-07-16 13:06:54 -0400

    Get version to update when the package.json value changes. Update to 2.5.1

--- src/components/custom/game/components/version.tsx
+++ src/components/custom/game/components/version.tsx
@@ -4,16 +4,18 @@ import { Button } from '@/components/ui/button';
 import { NavLink } from 'react-router-dom';
 
 export const Version: FC = () => {
+	const changelogByTag: string = `https://github.com/hxvry-dev/site/compare/${__APP_VERSION__}...main`;
+
 	return (
 		<div className="justify-self-center mt-5">
 			<TooltipProvider>
 				<Tooltip>
 					<TooltipTrigger asChild>
-						<small className="code mb-2">Version: 2.5.0</small>
+						<small className="code mb-2">Version: {__APP_VERSION__}</small>
 					</TooltipTrigger>
 					<TooltipContent className="bg-background border-2 text-foreground max-w-[240px] overflow-auto">
 						<Button asChild variant="ghost" className="rounded-md">
-							<NavLink to="https://github.com/hxvry-dev/site/compare/v2.0.0...main" target="_blank">
+							<NavLink to={changelogByTag} target="_blank">
 								Changelog
 							</NavLink>
 						</Button>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import json from './package.json';
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	define: {
+		__APP_VERSION__: JSON.stringify(json.version),
+	},
 	build: {
 		minify: 'terser',
 		rollupOptions: {
