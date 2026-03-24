54898bcdd3f6fe7f13c88f81eba07de0d22bf355
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-03-24 17:00:52 -0400

    Fix Build

--- src/components/custom/game/components/OfflineProgressModal.tsx
+++ src/components/custom/game/components/OfflineProgressModal.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { FC } from 'react';
 import { costFormatter } from './util/util';
