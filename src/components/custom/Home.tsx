1be81f2895eb44f40226a28580b9e744d7378af5
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-03-26 00:53:01 -0400

    Update Home Layout

--- src/components/custom/Home.tsx
+++ src/components/custom/Home.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
 import { useIsMobile } from '@/hooks/use-mobile';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/accordion';
 
 const Home = () => {
 	const [isHovering, setIsHovering] = useState(false);
@@ -10,60 +11,45 @@ const Home = () => {
 
 	return (
 		<div className="grid mx-auto w-fit font-mono">
-			<div
-				className={
-					isMobile
-						? 'mt-32 p-5 border-2 rounded-xs max-w-[650px] w-fit'
-						: 'mt-64 p-5 border-2 rounded-xs max-w-[650px] w-fit'
-				}
-			>
-				<h1>Hi, I&apos;m Henry!</h1>
+			<div className={isMobile ? 'mt-32 p-5 min-w-fit max-w-[600px]' : 'mt-64 p-5 min-w-fit max-w-[600px]'}>
+				<h1 className="grid mx-auto">Hi, I&apos;m Henry. Welcome to my website.</h1>
 				<br />
-				<h2>
-					I like to write code, talk about cars, doom scroll social media, and play video games in my free
-					time. Welcome to my website.
-				</h2>
+				<h2>I like to write code, talk about cars, and play video games in my free time.</h2>
 			</div>
-			<h4 className="pt-5 pb-5 pl-5 pr-5 w-fit mx-auto">
-				Okay, now that we&apos;ve gotten that out of the way...
-			</h4>
-			<h4 className="pb-5 pl-5 pr-5 w-fit mx-auto">Check out these links!</h4>
-			<div
-				className={
-					isMobile
-						? 'grid grid-cols-1 grid-rows-5 gap-5 w-fit mx-auto'
-						: 'grid grid-cols-4 grid-rows-2 gap-5 w-fit mx-auto'
-				}
-			>
-				<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
-					<NavLink to="https://www.linkedin.com/in/henry-ouellette-8a3b36201/" target="_blank">
-						My LinkedIn
-					</NavLink>
-				</Button>
-				<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
-					<NavLink to="/resume">My Resume</NavLink>
-				</Button>
-				<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
-					<NavLink to="https://github.com/hxvry-dev" target="_blank">
-						My Github
-					</NavLink>
-				</Button>
-				<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
-					<NavLink to="https://bsky.app/profile/hxvry.com" target="_blank">
-						My Bluesky
-					</NavLink>
-				</Button>
-				<Button
-					asChild
-					size="sm"
-					variant={isMobile ? 'outline' : 'ghost'}
-					className={isMobile ? '' : 'col-span-4'}
-				>
-					<NavLink to="/projects">Projects</NavLink>
-				</Button>
-			</div>{' '}
+			<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'} className="grid mx-auto max-w-fit px-5">
+				<NavLink to="/projects">Personal Projects + Games</NavLink>
+			</Button>
+			<div className="max-w-fit mx-auto">
+				<Accordion type="single" collapsible>
+					<AccordionItem value="get-in-touch">
+						<AccordionTrigger className="text-muted-foreground max-w-fit mx-auto">
+							Get in Touch!
+						</AccordionTrigger>
+						<AccordionContent>
+							<div className="grid grid-flow-col min-w-fit mx-auto">
+								<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
+									<NavLink
+										to="https://www.linkedin.com/in/henry-ouellette-8a3b36201/"
+										target="_blank"
+									>
+										My LinkedIn
+									</NavLink>
+								</Button>
+								<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
+									<NavLink to="/resume">My Resume</NavLink>
+								</Button>
+								<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
+									<NavLink to="https://github.com/hxvry-dev" target="_blank">
+										My Github
+									</NavLink>
+								</Button>
+							</div>
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
+			</div>
 			<small className="mx-auto text-stone-700 italic">
-				Made with &#9829; by me, in the{' '}
+				Made with &#9829; by me in the{' '}
 				<span onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
 					{isHovering ? 'Flour' : 'Flower'}
 				</span>{' '}
