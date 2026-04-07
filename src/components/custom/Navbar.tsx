d1e30a91dcd079d1bf966f9cb28f147b3e468533
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-04-07 15:54:56 -0400

    Navbar re-write

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,25 +1,56 @@
-import { NavLink } from 'react-router-dom';
-
-import { Button } from '@/components/ui/button';
+import { Location, useLocation } from 'react-router-dom';
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from '../ui/breadcrumb';
 import { ThemeDropdown } from './ThemeDropdown';
+import { useIsMobile } from '@/hooks/use-mobile';
 
-const Navbar = () => {
+interface CrumbProps {
+	loc: Location;
+}
+
+const Crumb = ({ loc }: CrumbProps) => {
+	const path = loc.pathname.split('/');
+	const isMobile = useIsMobile();
 	return (
-		<div className="font-bold font-mono grid grid-cols-5 gap-2">
-			<Button
-				asChild
-				size="icon"
-				variant="ghost"
-				className={`bg-background text-foreground hover:bg-accent hover:text-accent-foreground`}
-			>
-				<NavLink to="/" className="rounded-none">
-					[HO]
-				</NavLink>
-			</Button>
-			<h1 className="place-self-center col-span-3">hxvry</h1>
-			<ThemeDropdown />
-		</div>
+		<Breadcrumb>
+			<BreadcrumbList>
+				<div className={isMobile ? 'flex items-center' : 'flex items-center space-x-2'}>
+					<BreadcrumbItem>
+						<BreadcrumbLink href={`/${path[0]}`}>Home</BreadcrumbLink>
+					</BreadcrumbItem>
+					{loc.pathname === '/' ? null : <BreadcrumbSeparator />}
+					{path
+						.filter((item) => item !== '')
+						.map((i) => (
+							<div key={i}>
+								<BreadcrumbItem>
+									<BreadcrumbPage>{i.at(0)?.toLocaleUpperCase() + i.slice(1)}</BreadcrumbPage>
+								</BreadcrumbItem>
+							</div>
+						))}
+				</div>
+			</BreadcrumbList>
+		</Breadcrumb>
 	);
 };
 
-export default Navbar;
+export const Navbar = () => {
+	const loc = useLocation();
+	return (
+		<div className="grid grid-cols-3 font-mono text-center text-md">
+			<div className="self-center px-3 col-span-1">
+				<Crumb loc={loc} />
+			</div>
+			<span className="mx-auto self-center col-span-1">Henry Ouellette</span>
+			<div className="col-span-1">
+				<ThemeDropdown />
+			</div>
+		</div>
+	);
+};
