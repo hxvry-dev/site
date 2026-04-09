666bce8f43c7565e8227eed864f26abd4c0feb65
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-04-09 04:08:08 -0400

    Prep for Home overhaul

--- src/components/custom/Home.tsx
+++ src/components/custom/Home.tsx
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
 import { useIsMobile } from '@/hooks/use-mobile';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/accordion';
 
 const Home = () => {
 	const [isHovering, setIsHovering] = useState(false);
@@ -19,35 +18,6 @@ const Home = () => {
 			<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'} className="grid mx-auto max-w-fit px-5">
 				<NavLink to="/projects">Personal Projects + Games</NavLink>
 			</Button>
-			<div className="max-w-fit mx-auto">
-				<Accordion type="single" collapsible>
-					<AccordionItem value="get-in-touch">
-						<AccordionTrigger className="text-muted-foreground max-w-fit mx-auto">
-							Get in Touch!
-						</AccordionTrigger>
-						<AccordionContent>
-							<div className="grid grid-flow-col min-w-fit mx-auto">
-								<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
-									<NavLink
-										to="https://www.linkedin.com/in/henry-ouellette-8a3b36201/"
-										target="_blank"
-									>
-										My LinkedIn
-									</NavLink>
-								</Button>
-								<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
-									<NavLink to="/resume">My Resume</NavLink>
-								</Button>
-								<Button asChild size="sm" variant={isMobile ? 'outline' : 'ghost'}>
-									<NavLink to="https://github.com/hxvry-dev" target="_blank">
-										My Github
-									</NavLink>
-								</Button>
-							</div>
-						</AccordionContent>
-					</AccordionItem>
-				</Accordion>
-			</div>
 			<small className="mx-auto text-stone-700 italic">
 				Made with &#9829; by me in the{' '}
 				<span onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
