3e792f922a7a79d0ec0d246d21970d67a0104b29
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-04-09 02:17:53 -0400

    Prepare to update forms

--- src/components/custom/game/components/version.tsx
+++ src/components/custom/game/components/version.tsx
@@ -1,4 +1,3 @@
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { Button } from '@/components/ui/button';
 import { NavLink } from 'react-router-dom';
 
@@ -7,20 +6,11 @@ export const Version = () => {
 
 	return (
 		<div className="justify-self-center mt-5">
-			<TooltipProvider>
-				<Tooltip>
-					<TooltipTrigger asChild>
-						<small className="code mb-2">Version: {__APP_VERSION__}</small>
-					</TooltipTrigger>
-					<TooltipContent className="bg-background border-2 text-foreground max-w-[240px] overflow-auto">
-						<Button asChild variant="ghost" className="rounded-md">
-							<NavLink to={changelogByTag} target="_blank">
-								Changelog
-							</NavLink>
-						</Button>
-					</TooltipContent>
-				</Tooltip>
-			</TooltipProvider>
+			<Button variant="link" asChild>
+				<NavLink to={changelogByTag} target="_blank">
+					<small className="code mb-2 p-1">Version: {__APP_VERSION__}</small>
+				</NavLink>
+			</Button>
 		</div>
 	);
 };
