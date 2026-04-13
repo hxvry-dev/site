dd0534a0529e8a67390590fa81eab2bf28bb5df3
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-04-13 15:30:25 -0400

    Add larger size for mobile buttons.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
 				xs: "h-6 gap-1 px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
 				sm: 'h-8 gap-1.5 px-3 has-[>svg]:px-2.5',
 				lg: 'h-10 px-6 has-[>svg]:px-4',
+				xl: 'h-12 px-8 has-[>svg]:px-6',
 				icon: 'size-9',
 				'icon-xs': "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
 				'icon-sm': 'size-8',
