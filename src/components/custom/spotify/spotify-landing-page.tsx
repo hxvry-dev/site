cbb521072a53ad01d67854f660c284aa8e07a99e
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-04-16 17:20:18 -0400

    Mobile-ify the Spotify landing page.

--- src/components/custom/spotify/spotify-landing-page.tsx
+++ src/components/custom/spotify/spotify-landing-page.tsx
@@ -1,14 +1,22 @@
 import { TopArtistsResponse, TopTracksResponse } from './spotify-card';
 import { SpotifyTopArtistsCard, SpotifyTopTracksCard } from './spotify-card';
 
+import { useIsMobile } from '@/hooks/use-mobile';
 import { useSpotify } from '@/hooks/use-spotify';
 
 export const SpotifyLandingPage = () => {
 	const { data: trackData } = useSpotify<TopTracksResponse>('/me/top/tracks?limit=5');
 	const { data: artistData } = useSpotify<TopArtistsResponse>('/me/top/artists?limit=5');
+	const isMobile = useIsMobile();
 
 	return (
-		<div className="w-full px-5 grid grid-cols-2 gap-5 mx-auto font-mono">
+		<div
+			className={
+				isMobile
+					? 'flex flex-col font-mono gap-5 mx-auto max-w-fit'
+					: 'grid grid-cols-2 font-mono gap-5 mx-auto max-w-fit'
+			}
+		>
 			<div>
 				<h2 className="my-5 text-center">Top Tracks:</h2>
 				<SpotifyTopTracksCard data={trackData!} />
