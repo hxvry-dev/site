8f8deb1af745d5d0ce4fce551b7aa6cc280962fd
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-04-21 16:16:44 -0400

    Add Login with Spotify button to the Spotify landing page.

--- src/components/custom/spotify/spotify-landing-page.tsx
+++ src/components/custom/spotify/spotify-landing-page.tsx
@@ -1,8 +1,10 @@
 import { TopArtistsResponse, TopTracksResponse } from './spotify-card';
 import { SpotifyTopArtistsCard, SpotifyTopTracksCard } from './spotify-card';
 
+import { Button } from '@/components/ui/button';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { useSpotify } from '@/hooks/use-spotify';
+import { loginWithSpotify } from '@/lib/spotify-auth';
 
 export const SpotifyLandingPage = () => {
 	const { data: trackData } = useSpotify<TopTracksResponse>('/me/top/tracks?limit=5');
@@ -10,18 +12,25 @@ export const SpotifyLandingPage = () => {
 	const isMobile = useIsMobile();
 
 	return (
-		<div
-			className={
-				isMobile
-					? 'flex flex-col font-mono gap-5 mx-auto max-w-fit'
-					: 'grid grid-cols-2 font-mono gap-5 mx-auto max-w-fit'
-			}
-		>
-			<div>
+		<div className={'grid grid-cols-2 font-mono gap-5 mx-auto max-w-fit'}>
+			<div className="mx-auto col-span-2 row-end-1">
+				<Button
+					size={isMobile ? 'xl' : 'sm'}
+					variant="outline"
+					className="px-5 max-w-fit"
+					onClick={() => {
+						sessionStorage.clear();
+						loginWithSpotify();
+					}}
+				>
+					Log In To View/Refresh Your Spotify Stats
+				</Button>
+			</div>
+			<div className="col-span-1">
 				<h2 className="my-5 text-center">Top Tracks:</h2>
 				<SpotifyTopTracksCard data={trackData!} />
 			</div>
-			<div>
+			<div className="col-span-1">
 				<h2 className="my-5 text-center">Top Artists:</h2>
 				<SpotifyTopArtistsCard data={artistData!} />
 			</div>
