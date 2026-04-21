74f46cbcbdcae01661207a788fbee1036bff4249
hxvry-dev <72309050+hxvry-dev@users.noreply.github.com>
2026-04-21 16:17:48 -0400

    Update useSpotify and SpotifyCallback.
    They use the Auth context now.

--- src/components/custom/spotify/spotify-callback.tsx
+++ src/components/custom/spotify/spotify-callback.tsx
@@ -3,14 +3,18 @@ import { useNavigate } from 'react-router-dom';
 
 import { fetchAccessToken } from '../../../lib/spotify-auth';
 
+import { useSpotifyAuth } from '@/context/spotify-auth-context';
+
 export const SpotifyCallback = () => {
 	const nav = useNavigate();
+	const { setAccessToken, setRefreshToken } = useSpotifyAuth();
+
 	useEffect(() => {
 		const code = new URLSearchParams(window.location.search).get('code');
 		if (code) {
 			fetchAccessToken(code).then(({ access_token, refresh_token }) => {
-				sessionStorage.setItem('access_token', access_token);
-				sessionStorage.setItem('refresh_token', refresh_token);
+				setAccessToken(access_token);
+				setRefreshToken(refresh_token!);
 				nav('/spotify');
 			});
 		}
